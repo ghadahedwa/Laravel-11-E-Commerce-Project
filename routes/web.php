a4de0c8f43c28e8cab2596623824f9bd4241b60a
Ghada Mohammed <ghadahedwa.gmail.com>
2024-11-09 00:03:17 +0200

    Project & Layout Setup

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');

a524c08b56d2767c2f643851b143e334c1dddf84
Ghada Mohammed <ghadahedwa.gmail.com>
2024-11-13 00:22:44 +0200

    Changing the Login and Registration Page Layout

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
+use App\Http\Middleware\AuthAdmin;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,11 +14,11 @@ Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/account.dashboard',[UserController::class,'index'])->name('user.index');
 });
 
-Route::middleware(['auth',\App\Http\Middleware\AuthAdmin::class])->group(function(){
+Route::middleware(['auth',AuthAdmin::class])->group(function(){
     Route::get('/admin',[AdminController::class,'index'])->name('admin.index');
 });
 
